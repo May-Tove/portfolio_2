4aa903c270caf063e4ca74e1572bcc1546ddbe9a
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-06-11 13:33:02 +0200

    Update App.jsx with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import './styles.scss';
+import { createRef } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import ProjectsList from './components/ProjectsList';
+import { AnimatePresence } from 'framer-motion';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
+  const aboutRef = createRef();
+  const projectsRef = createRef();
+  const contactRef = createRef();
   return (
     <>
-      <h1>Hello test</h1>
+      <AnimatePresence mode="wait">
+        <Header
+          projectsRef={projectsRef}
+          aboutRef={aboutRef}
+          contactRef={contactRef}
+        />
+        <main>
+          <Hero />
+          <div className="main-global-styling">
+            <ProjectsList projectsRef={projectsRef} />
+            <About aboutRef={aboutRef} />
+            <Contact contactRef={contactRef} />
+          </div>
+        </main>
+        <Footer />
+      </AnimatePresence>
     </>
   );
 }
