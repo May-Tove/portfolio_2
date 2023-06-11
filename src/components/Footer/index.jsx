2be93d6d37b02f2cc3fe668be8d828c4f4004822
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-06-11 13:34:35 +0200

    Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import * as Styled from './index.styles';
+
+const Footer = () => {
+  return (
+    <Styled.FooterContainer>
+      <p>Thanks for visiting my portfolio!</p>
+    </Styled.FooterContainer>
+  );
+};
+export default Footer;
---
+++ src/components/Footer/index.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  text-align: center;
+  width: 100%;
+  padding: 2rem;
+`;
